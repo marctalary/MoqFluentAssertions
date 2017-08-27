a1f3637f1506b1d49ddb347ad960b5f22d6c8b37
Marc Talary <marc@talary.co.uk>
2017-08-27 02:53:09 +0100

    Add parameter to support moq setups annd verifications more than once on the same method, probably the ony sensible use but prevents field level feedback

--- MoqFluentAssertions/Its.cs
+++ MoqFluentAssertions/Its.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System;
 
 namespace MoqFluentAssertions
 {
@@ -9,7 +10,15 @@ namespace MoqFluentAssertions
         /// </summary>
         public static T EquivalentTo<T>(T expected)
         {
-            return Moq.It.Is<T>(actual => actual.ShouldBeEquivalentToReturnSuccess(expected));
+            return Moq.It.Is<T>(actual => actual.ShouldBeEquivalentToReturnSuccess(expected, false));
+        }
+
+        /// <summary>
+        /// Note: can only be used if there is only one call to the method being verified
+        /// </summary>
+        public static T EquivalentTo<T>(T expected, bool allowMultipleSetups)
+        {
+            return Moq.It.Is<T>(actual => actual.ShouldBeEquivalentToReturnSuccess(expected, allowMultipleSetups));
         }
 
         /// <summary>
@@ -17,7 +26,15 @@ namespace MoqFluentAssertions
         /// </summary>
         public static TActual EquivalentTo<TActual, TExpected>(TExpected expected)
         {
-            return Moq.It.Is<TActual>(actual => actual.ShouldBeEquivalentToReturnSuccess(expected));
+            return EquivalentTo<TActual, TExpected>(expected, false);
+        }
+
+        /// <summary>
+        /// Note: can only be used if there is only one call to the method being verified
+        /// </summary>
+        public static TActual EquivalentTo<TActual, TExpected>(TExpected expected, bool allowMultipleSetups)
+        {
+            return Moq.It.Is<TActual>(actual => actual.ShouldBeEquivalentToReturnSuccess(expected, allowMultipleSetups));
         }
 
         /// <summary>
@@ -25,14 +42,32 @@ namespace MoqFluentAssertions
         /// For use in Moq verify 
         /// e.g. mock.Verify(t => s.TomeMethod(Moq.It.Is(o=>o.ShouldBeEquivalentToReturnSuccess(expected))))
         /// </summary>
-        /// <typeparam name="T1Value"></typeparam>
-        /// <typeparam name="T2Value"></typeparam>
-        /// <param name="expected"></param>
-        /// <param name="actual"></param>
-        /// <returns></returns>
-        public static bool ShouldBeEquivalentToReturnSuccess<T1Value, T2Value>(this T1Value expected, T2Value actual)
+        public static bool ShouldBeEquivalentToReturnSuccess<T1Value, T2Value>(this T1Value actual, T2Value expected)
         {
-            actual.ShouldBeEquivalentTo(expected);
+            return actual.ShouldBeEquivalentToReturnSuccess(expected, false);
+        }
+
+        /// <summary>
+        /// Calls fuent assert should be equivilent to and returns true or throws an assertion exception details the property mismatch.
+        /// For use in Moq verify 
+        /// e.g. mock.Verify(t => s.TomeMethod(Moq.It.Is(o=>o.ShouldBeEquivalentToReturnSuccess(expected))))
+        /// </summary>
+        public static bool ShouldBeEquivalentToReturnSuccess<T1Value, T2Value>(this T1Value actual, T2Value expected, bool allowMultipleSetups)
+        {
+            if (allowMultipleSetups)
+            {
+                try
+                {
+                    actual.ShouldBeEquivalentTo(expected);
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+                actual.ShouldBeEquivalentTo(expected);
+
             return true;
         }
     }
--- MoqFluentAssertionsTests/EquivilentToTests.cs
+++ MoqFluentAssertionsTests/EquivilentToTests.cs
@@ -69,35 +69,36 @@ namespace MoqFluentAssertionsTests
             var booMock = new Mock<IBar>();
             var foo = new Foo(booMock.Object);
             var fooParam = new FooParam { Description = "test description", ReferenceNumber = 12345 };
-            var fooParamMisMatch = new FooParam { Description = "tesdst description", ReferenceNumber = 123345 };
+            var expectedFooParam = new FooParam { Description = "tesdst description", ReferenceNumber = 123345 };
             foo.CallBoo(fooParam);
 
             // When
             var testException = Assert.Throws<XunitException>(() =>
-                booMock.Verify(b => b.ItWorked(It.Is<BarParam>(o => o.ShouldBeEquivalentToReturnSuccess(fooParamMisMatch))))
+                booMock.Verify(b => b.ItWorked(It.Is<BarParam>(o => o.ShouldBeEquivalentToReturnSuccess(expectedFooParam))))
             );
 
             // Then 
             var expectedStartOfException =
-                $"Expected member {nameof(fooParam.Description)} to be {Environment.NewLine}"
-                + $"\"{fooParam.Description}\" with a length of {fooParam.Description.Length}, but {Environment.NewLine}"
-                + $"\"{fooParamMisMatch.Description}\" has a length of {fooParamMisMatch.Description.Length}.{Environment.NewLine}"
-                + $"Expected member {nameof(fooParam.ReferenceNumber)} to be {fooParam.ReferenceNumber}, "
-                + $"but found {fooParamMisMatch.ReferenceNumber}.";
+                $"Expected member {nameof(expectedFooParam.Description)} to be {Environment.NewLine}"
+                + $"\"{expectedFooParam.Description}\" with a length of {expectedFooParam.Description.Length}, but {Environment.NewLine}"
+                + $"\"{fooParam.Description}\" has a length of {fooParam.Description.Length}.{Environment.NewLine}"
+                + $"Expected member {nameof(expectedFooParam.ReferenceNumber)} to be {expectedFooParam.ReferenceNumber}, "
+                + $"but found {fooParam.ReferenceNumber}.";
             Assert.StartsWith(expectedStartOfException, testException.Message);
         }
 
         [Fact]
-        public void Its_EquivalentTo_DoesNotThrowException()
+        public void Its_EquivalentTo_WithDiffereningTypes_DoesNotThrowException()
         {
             // Given
             var booMock = new Mock<IBar>();
             var foo = new Foo(booMock.Object);
             var fooParam = new FooParam { Description = "test description", ReferenceNumber = 12345 };
+            var expectedFooParam = new FooParam { Description = fooParam.Description, ReferenceNumber = fooParam.ReferenceNumber };
             foo.CallBoo(fooParam);
 
             // When
-            booMock.Verify(b => b.ItWorked(Its.EquivalentTo<BarParam, FooParam>(fooParam)));
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo<BarParam, FooParam>(expectedFooParam)));
 
             // Then 
             // No Exception thrown
@@ -109,22 +110,39 @@ namespace MoqFluentAssertionsTests
             // Given
             var booMock = new Mock<IBar>();
             var foo = new Foo(booMock.Object);
-            var fooParam = new FooParam { Description = "test description", ReferenceNumber = 12345 };
-            var fooParamMisMatch = new FooParam { Description = "tesdst description", ReferenceNumber = 123345 };
+            var fooParam = new FooParam { Description = "actual description", ReferenceNumber = 12345 };
+            var expectedFooParam = new FooParam { Description = "expected description", ReferenceNumber = 54321 };
             foo.CallBoo(fooParam);
 
             // When
             var testException = Assert.Throws<XunitException>(() =>
-                booMock.Verify(b => b.ItWorked(Its.EquivalentTo<BarParam, FooParam>(fooParamMisMatch)))
+                booMock.Verify(b => b.ItWorked(Its.EquivalentTo<BarParam, FooParam>(expectedFooParam)))
             );
 
             // Then 
             var expectedStartOfException =
-                $"Expected member {nameof(fooParam.Description)} to be {Environment.NewLine}\"{fooParam.Description}\" with a length of {fooParam.Description.Length}, "
-                + $"but {Environment.NewLine}\"{fooParamMisMatch.Description}\" has a length of {fooParamMisMatch.Description.Length}."
-                + $"{Environment.NewLine}Expected member {nameof(fooParam.ReferenceNumber)} to be {fooParam.ReferenceNumber}, "
-                + $"but found {fooParamMisMatch.ReferenceNumber}.";
+                $"Expected member {nameof(expectedFooParam.Description)} to be {Environment.NewLine}\"{expectedFooParam.Description}\" with a length of {expectedFooParam.Description.Length}, "
+                + $"but {Environment.NewLine}\"{fooParam.Description}\" has a length of {fooParam.Description.Length}."
+                + $"{Environment.NewLine}Expected member {nameof(expectedFooParam.ReferenceNumber)} to be {expectedFooParam.ReferenceNumber}, "
+                + $"but found {fooParam.ReferenceNumber}.";
             Assert.StartsWith(expectedStartOfException, testException.Message);
+        }
+
+        [Fact]
+        public void Its_EquivalentTo_DoesNotThrowException()
+        {
+            // Given
+            var booMock = new Mock<IBar>();
+            var foo = new Foo(booMock.Object);
+            var fooParam = new FooParam { Description = "test description", ReferenceNumber = 12345 };
+            var expectedBarParam = new BarParam { Description = fooParam.Description, ReferenceNumber = fooParam.ReferenceNumber };
+            foo.CallBoo(fooParam);
+
+            // When
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo(expectedBarParam)));
+
+            // Then 
+            // No Exception thrown
         }
 
         [Fact]
@@ -133,23 +151,74 @@ namespace MoqFluentAssertionsTests
             // Given
             var booMock = new Mock<IBar>();
             var foo = new Foo(booMock.Object);
-            var barParam = new BarParam { Description = "test description", ReferenceNumber = 12345 };
-            var barParamMisMatch = new BarParam { Description = "tesdst description", ReferenceNumber = 123345 };
+            var expectedBarParam = new BarParam { Description = "expected description", ReferenceNumber = 12345 };
+            var barParam = new BarParam { Description = "actual description", ReferenceNumber = 54321 };
             foo.CallBoo(barParam);
 
             // When
             var testException = Assert.Throws<XunitException>(() =>
-                booMock.Verify(b => b.ItWorked(Its.EquivalentTo(barParamMisMatch)))
+                booMock.Verify(b => b.ItWorked(Its.EquivalentTo(expectedBarParam)))
             );
 
             // Then 
             var expectedStartOfException =
-                $"Expected member {nameof(barParam.Description)} to be {Environment.NewLine}\"{barParam.Description}\" with a length of {barParam.Description.Length}, "
-                + $"but {Environment.NewLine}\"{barParamMisMatch.Description}\" has a length of {barParamMisMatch.Description.Length}."
-                + $"{Environment.NewLine}Expected member {nameof(barParam.ReferenceNumber)} to be {barParam.ReferenceNumber}, "
-                + $"but found {barParamMisMatch.ReferenceNumber}.";
+                $"Expected member {nameof(expectedBarParam.Description)} to be {Environment.NewLine}\"{expectedBarParam.Description}\" with a length of {expectedBarParam.Description.Length}, "
+                + $"but {Environment.NewLine}\"{barParam.Description}\" has a length of {barParam.Description.Length}."
+                + $"{Environment.NewLine}Expected member {nameof(expectedBarParam.ReferenceNumber)} to be {expectedBarParam.ReferenceNumber}, "
+                + $"but found {barParam.ReferenceNumber}.";
 
             Assert.StartsWith(expectedStartOfException, testException.Message);
+        }
+
+        [Fact]
+        public void Its_EquivalentTo_DoesNotThrowException_WithMultipleSetups()
+        {
+            // Given
+            var booMock = new Mock<IBar>();
+            var foo = new Foo(booMock.Object);
+            var fooParam = new FooParam { Description = "test description", ReferenceNumber = 12345 };
+            var expectedBarParam = new BarParam { Description = fooParam.Description, ReferenceNumber = fooParam.ReferenceNumber };
+
+            booMock.Setup(b => b.ItWorked(Its.EquivalentTo(expectedBarParam, true)));
+            booMock.Setup(b => b.ItWorked(Its.EquivalentTo(new BarParam(), true)));
+            foo.CallBoo(fooParam);
+            foo.CallBoo(new FooParam());
+
+            // When
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo(expectedBarParam, true)), Times.Once);
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo(new BarParam(), true)), Times.Once);
+
+            // Then 
+            // No Exception thrown
+        }
+
+        [Fact]
+        public void Its_EquivalentTo_ThrowsException_WithMultipleSetups()
+        {
+            // Given
+            var booMock = new Mock<IBar>();
+            var foo = new Foo(booMock.Object);
+            var fooParam = new FooParam { Description = "given value 1", ReferenceNumber = 12345 };
+            var fooParam2 = new FooParam { Description = "given value 2", ReferenceNumber = 23456 };
+            var expectedBarParam = new BarParam { Description = fooParam.Description, ReferenceNumber = fooParam.ReferenceNumber };
+            var expectedBarParam2 = new BarParam { Description = fooParam2.Description, ReferenceNumber = fooParam2.ReferenceNumber };
+            var unExpectedBarParam = new BarParam { Description = "Unexpected value" };
+
+            booMock.Setup(b => b.ItWorked(Its.EquivalentTo(expectedBarParam, true)));
+            booMock.Setup(b => b.ItWorked(Its.EquivalentTo(expectedBarParam2, true)));
+            foo.CallBoo(fooParam);
+            foo.CallBoo(fooParam2);
+
+            // When
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo(expectedBarParam, true)), Times.Once);
+            booMock.Verify(b => b.ItWorked(Its.EquivalentTo(expectedBarParam2, true)), Times.Once);
+
+            // and When Then
+            var testException = Assert.Throws<MockException>(() =>
+                booMock.Verify(b => b.ItWorked(Its.EquivalentTo(unExpectedBarParam, true)), Times.Once)
+            );
+
+            Assert.StartsWith("Expected invocation on the mock once, but was 0 times", testException.Message.Trim());
         }
     }
 }
